dd80e87a47bc4462b3bcb3d384b60da4df288459
Hixmixly <152249445+Hixmixly@users.noreply.github.com>
2024-02-21 23:43:32 -0800

    added fetch

--- public/index.js
+++ public/index.js
@@ -1,9 +1,34 @@
-async function main() {
+let GME = result.GME
+let MSFT = result.MSFT
+let DIS = result.DIS
+let BTNX = result.BTNX
+
+
+
+// Bonus Note: 
+// Another way to write the above lines would to refactor it as:
+   // const {GME, MSFT, DIS, BTNX} = result 
+// This is an example of "destructuring" an object
+// "Destructuring" creates new variables from an object or an array
+async function main(cc6d2bf51ae14971a2282b1bc411dbad) {
 
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    const response = await fetch ()
+
+    const result = await response.json ()
+
+    const {GME, MSFT, DIS, BNTX} = result;
+
+   const stocks = [GME, MSFT, DIS, BNTX];
+
+   stocks.forEach (stock => stock.values.reverse())
+
+
 }
 
-main()
+
+
+
